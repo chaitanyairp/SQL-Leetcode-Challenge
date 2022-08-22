1423fcf8ec446404065c2acf3473a53f9fadcde1
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-08-22 21:31:15 +0530

    Update Product Price at a given date.sql

--- Medium/Product Price at a given date.sql
+++ Medium/Product Price at a given date.sql
@@ -56,3 +56,19 @@ select t2.product_id, coalesce(new_price,10) as price
 from t2 left join t1
 on t2.product_id = t1.product_id
 order by price desc
+
+
+
+My sol:
+-- Colud have used left join insted of union. Check this
+
+with products_curr as (
+	select product_id, new_price, change_date, row_number() over(partition by product_id order by change_date desc) as rn
+	from products 
+	where change_date <= '2019-08-16'
+	
+)
+select product_id, new_price as price from products_curr where rn = 1
+union
+select product_id, 10 as price from products
+where product_id not in (select product_id from products_curr);
