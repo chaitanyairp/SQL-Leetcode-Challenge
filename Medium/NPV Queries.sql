bab8e97af4df2f7213e1af34ecebd4659ce7a9e5
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-08-23 21:28:54 +0530

    Update NPV Queries.sql

--- Medium/NPV Queries.sql
+++ Medium/NPV Queries.sql
@@ -78,3 +78,11 @@ select q.id, q.year, coalesce(n.npv,0) as npv
 from queries q
 left join npv n
 on q.id = n.id and q.year=n.year
+
+
+My sol:
+
+select q.id, q.year, coalesce(n.npv, 0) as npv
+from queries q left join npv n on q.id = n.id and q.year = n.year
+
+
