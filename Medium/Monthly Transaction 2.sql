4b3d4da5edc7a203c209d2a0c1014477bf00e48e
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-08-23 22:04:54 +0530

    Update Monthly Transaction 2.sql

--- Medium/Monthly Transaction 2.sql
+++ Medium/Monthly Transaction 2.sql
@@ -88,3 +88,32 @@ from t3
 union
 select *
 from t4
+
+
+
+
+My sol:
+
+ select 
+	concat(year(trans_date), '-', format(month(trans_date), '00')) as month, 
+	country,
+	count(id) as trans_count,
+	sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+	sum(amount) as trans_total_amount,
+	sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+ from transactions
+ group by country, concat(year(trans_date), '-', format(month(trans_date), '00'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
