5a5503a98abd54afc910f03290066cf3ecbdd597
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-08-25 21:46:25 +0530

    Update Game Play Analysis 3.sql

--- Medium/Game Play Analysis 3.sql
+++ Medium/Game Play Analysis 3.sql
@@ -48,3 +48,12 @@ select player_id, event_date,
 sum(games_played) over(partition by player_id order by event_date) as games_played_so_far
 from activity
 order by 1,2
+
+
+select
+	player_id,
+	event_date,
+	games_played_so_far = (sum(games_played) over(partition by player_id order by event_date))
+from Activity_62
+order by player_id
+
