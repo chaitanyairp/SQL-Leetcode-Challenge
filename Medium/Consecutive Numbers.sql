e991ccaa6d1fc548b11bb9efe295a5a472c1c4e0
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-08-25 22:13:06 +0530

    Update Consecutive Numbers.sql

--- Medium/Consecutive Numbers.sql
+++ Medium/Consecutive Numbers.sql
@@ -28,3 +28,27 @@ lag(num) over() as prev,
 lead(num) over() as next
 from logs) a
 where a.num = a.prev and a.num=a.next
+
+My sol:
+with t1 as (
+select
+	id as id1,
+	num as num1,
+	lead(id,1) over(order by id) as id2,
+	lead(num,1) over(order by id) as num2,
+	lead(id,2) over(order by id) as id3,
+	lead(num,2) over(order by id) as num3
+from logs
+)
+select num1 as consecutive_nums
+from t1
+where num1 = num2 and num2 = num3 and id2 - id1 = 1 and id3 - id2 = 1
+
+Using join:
+select a.num
+from logs a inner join logs b on b.id - a.id = 1 and a.num = b.num
+inner join logs c on c.id - b.id = 1 and c.num = b.num
+
+
+
+
