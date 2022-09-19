03cbb4aa1a12f64d617aeba7c3382a93be1189f5
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-09-19 21:49:56 +0530

    Update Activity Participants.sql

--- Medium/Activity Participants.sql
+++ Medium/Activity Participants.sql
@@ -100,4 +100,14 @@ group by activity
 select activity from cte where rn1 != 1 and rn2 != 1;
 
 
+Prac:
+with cte as (
+select activity, count(*) as cnt
+from Friends
+group by activity
+)
+select activity
+from cte
+where cnt != (select max(cnt) from cte)
+and cnt != (select min(cnt) from cte)
 	
