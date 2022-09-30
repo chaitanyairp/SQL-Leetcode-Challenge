b02544906e61e33c5818ca65f5bf23f5c01c3527
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-09-30 20:53:58 +0530

    Update Actors who cooperated with Directors atleast three times.sql

--- Easy/Actors who cooperated with Directors atleast three times.sql
+++ Easy/Actors who cooperated with Directors atleast three times.sql
@@ -41,3 +41,11 @@ Select actor_id, director_id
 from actordirector
 group by actor_id, director_id
 having count(*)>=3
+
+My sol:
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3
+
+
