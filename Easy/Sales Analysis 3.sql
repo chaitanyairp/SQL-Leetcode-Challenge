975db8e451c005ae021d9005da7e610264704181
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-10-02 00:28:22 +0530

    Update Sales Analysis 3.sql

--- Easy/Sales Analysis 3.sql
+++ Easy/Sales Analysis 3.sql
@@ -63,3 +63,9 @@ in
 and
 a.product_id not in 
 (select product_id from sales where sale_date > '2019-03-31' or sale_date < '2019-01-01')
+
+
+My sol:
+select distinct s.product_id, p.product_name
+from Sales s inner join Product p on s.product_id = p.product_id
+where sale_date between '2019-01-01' and '2019-03-31'
