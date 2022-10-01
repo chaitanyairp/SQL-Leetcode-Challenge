d551b6e533aa315c9f56d8f67edd3ec35329fe29
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-10-01 22:00:02 +0530

    Update Game play analysis 2.sql

--- Easy/Game play analysis 2.sql
+++ Easy/Game play analysis 2.sql
@@ -50,3 +50,24 @@ With table1 as
 Select t.player_id, t.device_id
 from table1 as t
 where t.rk=1
+
+
+My sol:
+with cte as (
+select
+player_id,
+device_id,
+rank() over(partition by player_id order by event_date) as rn
+from Activity_9
+)
+select player_id, device_id
+from cte
+where rn = 1
+
+
+
+
+
+
+
+
