e8f96bc6a1e52559cced13f3f2da0ff786c4bd2a
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-10-01 21:34:48 +0530

    Update Immediate food delivery.sql

--- Easy/Immediate food delivery.sql
+++ Easy/Immediate food delivery.sql
@@ -43,3 +43,15 @@
 Select 
 Round(avg(case when order_date=customer_pref_delivery_date then 1 else 0 end)*100,2) as immediate_percentage
 from delivery
+
+
+My sol:
+select 
+immediate_percentage = round(avg(case when order_date = customer_pref_delivery_date then 1.0 else 0.0 end)*100,2)
+from Delivery
+
+
+
+
+
+
