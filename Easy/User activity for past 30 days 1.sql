f4a2c315c28ce78b1ab56f4ce5ba72871eae368a
chaitanyairp <56603804+chaitanyairp@users.noreply.github.com>
2022-10-02 21:35:17 +0530

    Update User activity for past 30 days 1.sql

--- Easy/User activity for past 30 days 1.sql
+++ Easy/User activity for past 30 days 1.sql
@@ -50,3 +50,12 @@ Select activity_date as day, count(distinct user_id) as active_users
 from activity
 where activity_date > '2019-06-26' and activity_date < '2019-07-27'
 group by activity_date
+
+
+My sol:
+
+select activity_date, count(distinct session_id)
+from activity
+where activity_date between '2019-06-27' and '2019-07-27'
+group by activity_date
+
